53c988d018fe532118284be443173ac262aa0eaa
Vamshi Prasad <vamvijkum@gmail.com>
2021-07-16 12:58:19 +0530

    Add repository and fetch all students

---
+++ src/main/java/ml/vamshiprasad/learnSpring/student/StudentRepository.java
@@ -0,0 +1,12 @@
+package ml.vamshiprasad.learnSpring.student;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.UUID;
+
+@Repository
+public interface StudentRepository extends JpaRepository<Student, UUID> {
+
+
+}
--- src/main/java/ml/vamshiprasad/learnSpring/student/StudentService.java
+++ src/main/java/ml/vamshiprasad/learnSpring/student/StudentService.java
@@ -1,5 +1,6 @@
 package ml.vamshiprasad.learnSpring.student;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.time.LocalDate;
@@ -10,9 +11,16 @@ import java.util.UUID;
 @Service
 public class StudentService {
 
+    private final StudentRepository studentRepository;
+
+    @Autowired
+    public StudentService(StudentRepository studentRepository) {
+        this.studentRepository = studentRepository;
+    }
+
     public List<Student> getStudents(){
-        return List.of(
-                new Student(UUID.randomUUID(), "P Vamshi Prasad", LocalDate.of(1999, Month.JULY, 11), 22, "vamvijkum@gmail.com")
-        );
+        return studentRepository.findAll();
     }
 }
+
+//                new Student(UUID.randomUUID(), "P Vamshi Prasad", LocalDate.of(1999, Month.JULY, 11), 22, "vamvijkum@gmail.com")
